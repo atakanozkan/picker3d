fd7a6dcdda7f2c9d6277a560cbb6084d224a6be8
Atakan Ozkan <aozkan20@ku.edu.tr>
2023-06-10 05:52:51 +0300

    add new picker

--- Assets/Scripts/Objects/Player.cs
+++ Assets/Scripts/Objects/Player.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,4 +7,6 @@ public class Player : MonoBehaviour
 {
     public float horizontalSpeed = 5.0f;
     public float forwardSpeed = 10.0f;
+
+  
 }
