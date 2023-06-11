0fe8897ce14fe7004f5b66fa4a83e0c853bc923c
Atakan Ozkan <aozkan20@ku.edu.tr>
2023-06-11 06:03:52 +0300

    add new platform

---
+++ Assets/Scripts/Models/Managers/CollectManager.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Windows;
+
+namespace Models.Managers
+{
+    public class CollectManager : MonoBehaviour
+    {
+        private List<Ball> collectedBalls;
+
+        private void OnCollisionEnter(Collision collision)
+        {
+           
+        }
+    }
+}
--- Assets/Scripts/Objects/Ball.cs
+++ Assets/Scripts/Objects/Ball.cs
@@ -1,18 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
-
-public class Ball : MonoBehaviour
+using Objects;
+public class Ball : Collectable
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
